2618339dd3031983a441d527cb48f2845a24e0a5
jouhs <jaouad.ouhs@gmail.com>
2017-02-19 21:47:28 +0100

    jouhs: ajout des test sous mockito

--- MockCalc/src/main/java/org/jouhs/myapp/CalService.java
+++ MockCalc/src/main/java/org/jouhs/myapp/CalService.java
@@ -3,6 +3,14 @@ package org.jouhs.myapp;
 public class CalService {
 	ICalculator cal;
 
+	public ICalculator getCal() {
+		return cal;
+	}
+
+	public void setCal(ICalculator cal) {
+		this.cal = cal;
+	}
+
 	public int adddTwoNumbers(int x, int y) {
 		return cal.add(x, y);
 	}
--- MockCalc/src/test/java/org/jouhs/myapp/CalServiceTest.java
+++ MockCalc/src/test/java/org/jouhs/myapp/CalServiceTest.java
@@ -1,14 +1,20 @@
 package org.jouhs.myapp;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import static org.mockito.Mockito.*;
 
 public class CalServiceTest {
 
 	CalService calService;
 	
+	@Before
 	public void setup() {
+		ICalculator cal = mock(ICalculator.class);
+		when(cal.add(3,3)).thenReturn(6);
 		calService = new CalService();
+		calService.setCal(cal);
 	}
 	
 	@Test
